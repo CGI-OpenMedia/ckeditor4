0bd867d445d56bea16a178c0063e0c77d0de9684
Abhishek Agrawal <aagrawal@annova.de>
2022-04-25 08:09:21 +0200

    plugin added and updated config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
     config.allowedContent = true;
     config.fontSize_sizes = '8/8pt;9/9pt;10/10pt;11/11pt;12/12pt;14/14pt;16/16pt;18/18pt;20/20pt;22/22pt;24/24pt;26/26pt;28/28pt;36/36pt;48/48pt';
     //config.font_names = 'Segoe UI/Segoe UI;' + config.font_names;
@@ -22,11 +22,19 @@ CKEDITOR.editorConfig = function( config ) {
     config.linkShowTargetTab = false;
     // font configuration
     config.font_names = 'Arial;Calibri;Comic Sans MS;Courier New;Georgia;Harrington;Lucida Sans Unicode;Mistral;Segoe UI;SimSun;Tahoma;Times New Roman;Trebuchet MS;Verdana;Vivaldi;'
-    config.font_style = { element: "font", attributes: { face: "#(family)" }, overrides: [{ element: "font", attributes: { face: null } }] };
+
+    // please see font_style desc: https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-font_style
+
+    if (CKEDITOR.useNewRtfHtmlConvertor) {
+        config.font_style = { element: "span", styles: { 'font-family': '#(family)' } };
+    }
+    else {
+        config.font_style = { element: "font", attributes: { face: "#(family)" }, overrides: [{ element: "font", attributes: { face: null } }] };
+    }
 
     // keep fullPage prop equal to True to always send ckeditor content to the server as a full HTML page (including <html>, <head>, and <body>)
     config.fullPage = true;
- 
+
     // important: when Microsoft.Exchange cenvertor is used, keep enterMode prop equal to CKEDITOR.ENTER_DIV. The enterMode sets the behavior of the Enter key.
     // important: when CKEDITOR.ENTER_P is used, Microsoft.Exchange creates an extra new line characters for each paragraph (affects the insert positions)
     // important: when Microsoft.Exchange converts HTML to RTF and back to HTML it replaces all paragraphs with divs
---
+++ plugins/caseinsensitivefonts/plugin.js
@@ -0,0 +1,68 @@
+﻿/// <reference path="../../../../scripts/lodash.js" />
+
+/*
+ * This plugin makes ckeditor insensitive to the letter cases in the configured font names.
+ * The gembox convertor changes all letters of font names to lowercase.
+ * The plugin is written to solve this problem caused by HTML<->RTF conversion.
+*/
+
+CKEDITOR.plugins.add('caseinsensitivefonts',
+    {
+        beforeInit: function (editor) {
+
+            var faceAttribute = 'face';
+            var fontFamilyStyle = 'font-family';
+
+            var configuredFontNames = editor.config.font_names.split(';');
+
+            function findConfiguredFontName(fontName) {
+
+                if (!fontName)
+                    return fontName;
+
+                var configuredFont = _.find(configuredFontNames, function (element) {
+                    return element.toLowerCase() === fontName.toLowerCase();
+                });
+
+                return configuredFont || fontName;
+            }
+
+            CKEDITOR.config.font_defaultLabel = findConfiguredFontName(CKEDITOR.config.font_defaultLabel);
+
+            // override ckeditor functions
+
+            if (!CKEDITOR.tools._parseCssText) {
+                // parseCssText is used by CKEDITOR to get font-family style
+                CKEDITOR.tools._parseCssText = CKEDITOR.tools.parseCssText;
+            }
+
+            if (!CKEDITOR.dom.element.prototype._getAttribute) {
+                // getAttribute is used by CKEDITOR to get face attribute
+                CKEDITOR.dom.element.prototype._getAttribute = CKEDITOR.dom.element.prototype.getAttribute;
+            }
+
+            // get face attribute
+            CKEDITOR.dom.element.prototype.getAttribute = function (attribute) {
+
+                var value = this._getAttribute(attribute);
+
+                if (attribute == faceAttribute) {
+                    value = findConfiguredFontName(value);
+                }
+
+                return value;
+            }
+
+            // get font-family style         
+            CKEDITOR.tools.parseCssText = function (styleText, normalize, nativeNormalize) {
+
+                var value = CKEDITOR.tools._parseCssText(styleText, normalize, nativeNormalize);
+
+                if (fontFamilyStyle in value) {
+                    value[fontFamilyStyle] = findConfiguredFontName(value[fontFamilyStyle]);
+                }
+
+                return value;
+            }
+        }
+    });
